534af27a9fee4cd0ef28ff04cb270d25098f4e21
Eric Georges <eric.georges@businessdecision.com>
2018-05-11 14:54:26 +0200

    Upgrade for prometheus 2.0, add logback metrics

--- src/main/java/de/codecentric/springboot/monitoring/Application.java
+++ src/main/java/de/codecentric/springboot/monitoring/Application.java
@@ -3,7 +3,12 @@ package de.codecentric.springboot.monitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class Application {
 
 	public static void main(String[] args) {
